bf426425e1ccb670e3f8670c4f7b2f4119fb890c
lqy57 <larryyang91@gmail.com>
2023-02-21 00:18:27 -0600

    fixed links

--- models/linksPage.js
+++ models/linksPage.js
@@ -1,18 +1,18 @@
 const links = [
     {
         title: "Apple Store",
-        link: "<a href='https://www.apple.com/store?afid=p238%7CsHI7bHWVr-dc_mtid_1870765e38482_pcrid_648108207837_pgrid_13945964767_pntwk_g_pchan__pexid__&cid=aos-us-kwgo-brand-apple-store--slid---product-'>store</a>"
+        link: "https://www.apple.com/store"
     },
 
     {
         title: "Iphone",
-        link: "<a href='https://www.apple.com/shop/buy-iphone'>Iphone</a>",
+        link: "https://www.apple.com/shop/buy-iphone",
         
     },
     
     {
         title: "Ipad",
-        link:  "<a href='https://www.apple.com/shop/buy-ipad'>Ipad</a>",
+        link:  "https://www.apple.com/shop/buy-ipad",
     }
 ]
 
